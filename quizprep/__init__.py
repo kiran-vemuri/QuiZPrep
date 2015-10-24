4a24b392866e66eabf9b92b9657b232bd7f99800
Kiran Vemuri <vemkiran@gmail.com>
2015-10-24 15:31:33 -0700

    returning questions

--- quizprep/__init__.py
+++ quizprep/__init__.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('learn', '/learn')
+    config.add_route('qparser', '/trivia')
     config.scan()
     return config.make_wsgi_app()
--- quizprep/views.py
+++ quizprep/views.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
 from pyramid.view import view_config
+
+#!-- Methods
+
+def nlp_talk(data):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('0.0.0.0', 9990))
+    #print s.recv(1024)
+    #for data in ['lisi', 'zhangsan', 'wangwu']:
+    #data=""
+    if data:
+        s.send(str(data))
+        data = False
+        qdata = s.recv(8192)
+    s.send('exit')
+    s.close()
+    return qdata
+
+
+
+
+#!-- Views
 
 
 @view_config(route_name='home', renderer='templates/quizprep.pt')
@@ -8,3 +33,12 @@ def my_view(request):
 @view_config(route_name='learn', renderer='templates/learn.pt')
 def learn_view(request):
     return {'Question': 'Answer'}
+
+@view_config(route_name='qparser', renderer='templates/trivia.pt')
+def qparser(request):
+    if request.method == "POST":
+        in_text = request.params['in_text']
+        ret_data = nlp_talk(in_text)
+    return {'out_text': ret_data}
+
+
