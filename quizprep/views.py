ea6930d92d4df9879c1a3bd4e54745431908d47b
Kiran Vemuri <vemkiran@gmail.com>
2015-10-25 04:49:52 -0700

    new template

--- quizprep/views.py
+++ quizprep/views.py
@@ -119,6 +119,6 @@ def pebble_trivia_json(request):
 
     q_dict = json.loads(q_dict)
     outkey = random.choice(q_dict.keys())
-    #out_dict = random.sample( q_dict.items(), 1 )[0]
 
     return {'question': outkey, 'answer':q_dict[outkey]}
+
